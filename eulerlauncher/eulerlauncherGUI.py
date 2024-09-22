099b8d42b1320ab7cfa5226bdffb0c00fa5ab091
向宸宇 <xiangchenyu@isrc.iscas.ac.cn>
2024-09-22 23:21:51 +0800

    add Tips in GUI py file.

--- eulerlauncher/eulerlauncherGUI.py
+++ eulerlauncher/eulerlauncherGUI.py
@@ -4,13 +4,14 @@ Description: EulerLauncher前端界面
 Tips:
 - take-snapshot与export-development-image功能由于主程序尚未实现，该前端界面在这两部分留空，实现后请补齐
     具体需要修改的位置为button_vmsnapshot_clicked和button_vmout_clicked两个函数
-    Line 619  未实现的提示，主程序实现后请删除此行代码
-    Line 636  实现后请在实际情况下修改该判断条件，就是按照正确执行后命令行的返回值特点来进行判断
-    Line 671  未实现的提示，主程序实现后请删除此行代码
-    Line 689  实现后请在实际情况下修改该判断条件，就是按照正确执行后命令行的返回值特点来进行判断
+    Line 620  未实现的提示，主程序实现该功能后请删除此行代码
+    Line 637  实现后请在实际情况下修改该判断条件，就是按照正确执行后命令行的返回值特点来进行判断
+    Line 672  未实现的提示，主程序实现该功能后请删除此行代码
+    Line 690  实现后请在实际情况下修改该判断条件，就是按照正确执行后命令行的返回值特点来进行判断
+- 由于主程序暂未对Linux提供支持，因此Linux系统下的终端SSH连接功能还未进行验证，修改请在Line 739处进行
 - 增删改功能请在主界面init_ui或四个界面生成函数(以widget_of开头)中增删改按钮等控件，然后增删改对应的槽函数即可。
 - 本代码中有许多通用方法，如do_cmd、table_process、create_massage_box、create_line等，可以复用
-    它们在Line 373、414、954、963
+    它们在Line 374、415、955、964
 - 请注意，本代码仅为前端界面，后端功能由EulerLauncherd提供，因此请确保整个EulerLauncher已经正确安装并配置好conf文件
 """
 
